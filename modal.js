0572e54ce2854509a75674869960a1768882a092
simin161 <simin.ra161.2018@uns.ac.rs>
2021-07-19 15:09:50 +0200

    forma za registraciju

--- modal.js
+++ modal.js
@@ -1,24 +1,49 @@
-var modal = document.getElementById("logInModal");
+var logInModal = document.getElementById("logInModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("logInOpenModal");
+var btnLogInOpen = document.getElementById("logInOpenModal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btnLogInOpen.onclick = function() {
+  logInModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  logInModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == logInModal) {
+    logInModal.style.display = "none";
+  }
+}
+
+var registerModal = document.getElementById("registerModal");
+
+// Get the button that opens the modal
+var btnRegisterOpen = document.getElementById("registerOpenModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal 
+btnRegisterOpen.onclick = function() {
+  registerModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  registerModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == registerModal) {
+    registerModal.style.display = "none";
   }
 }
