c75a46348e73f6ffa254be7af59d039ba3f89fb6
jandric162 <jandric.ra162.2018@uns.ac.rs>
2021-08-22 00:47:24 +0200

    ispravka

--- static/components/index.js
+++ static/components/index.js
@@ -20,7 +20,7 @@ template: `<div>
 			<div>
 
 				<div class="lists" v-for="item in restaurats">
-					<span style="float: left; margin-top: 15px">
+					<span style="float: left;">
 						<img style="border-radius: 5px;" :src="item.restaurantLogo" height="90px" width="90px">
 
 					</span> 
